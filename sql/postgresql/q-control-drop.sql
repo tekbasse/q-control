9b86ee76e24daf486811d03aa32d994c5757ca54
Benjamin Brink <tekbasse@yahoo.com>
2016-10-01 02:54:40 -0400

    updated changes in tables, for teardown

--- sql/postgresql/q-control-drop.sql
+++ sql/postgresql/q-control-drop.sql
@@ -22,7 +22,7 @@ drop index qc_property_role_privilege_map_instance_id_idx;
 DROP TABLE qc_property_role_privilege_map;
 
 drop index qc_user_roles_map_qc_role_id_idx;
-drop index qc_user_roles_map_qal_customer_id_idx;
+drop index qc_user_roles_map_qal_contact_id_idx;
 drop index qc_user_roles_map_user_id_idx;
 drop index qc_user_roles_map_instance_id_idx;
 
@@ -30,7 +30,7 @@ DROP TABLE qc_user_roles_map;
 
 drop index qc_property_title_idx;
 drop index qc_property_id_idx;
-drop index qc_property_asset_type_id_idx;
+drop index qc_property_property_idx;
 drop index qc_property_instance_id_idx;
 
 DROP TABLE qc_property;
